7bb423c59b7ca95c1d6f6d25648abbd0e457f8a0
Rose1012 <rchowhan.0311@gmail.com>
2022-06-29 13:46:22 +0530

    python code for managing file

---
+++ file1.py
@@ -0,0 +1,3 @@
+Now the file has more content!
+ My name is Roshni, and my previous experience is with Microsoft as Support Engineer
+ My primary roles and responsibilities are working and assisting our clients with Azure DevOps related issues and educating them with the latest features and assisting them with various methods
